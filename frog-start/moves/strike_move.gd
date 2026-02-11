d69e0c71f4a2e748a8818602d27468deb37f9029
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-02-11 13:11:42 -0600

    foddian level started and level 5 completed

--- frog-start/moves/strike_move.gd
+++ frog-start/moves/strike_move.gd
@@ -1,13 +1,25 @@
 extends Node2D
 @onready var places: Node2D = $places
 @onready var strike_projectile: damager = $strikeProjectile
+@onready var area_2d: Area2D = $Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $Area2D/AnimatedSprite2D
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	strike_projectile.global_position=places.get_children()[randi_range(0,3)].global_position
+	area_2d.global_position=places.get_children()[randi_range(0,3)].global_position
 
 
 func _on_strike_projectile_area_entered(area: Area2D) -> void:
 	queue_free()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	area_2d.monitorable=false
+	animated_sprite_2d.play("default")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	strike_projectile.global_position=area_2d.global_position
+	
