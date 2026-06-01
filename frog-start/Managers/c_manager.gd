0f86bd7fb4f38faa9584c4e808e71a4902020696
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-05-31 20:47:49 -0500

    Reset character XP/levels and add leveling logic
    Update default character data: set charXP to varied starting XP values and reset all charLVL entries to 1 (replacing previously high level defaults). Add updateXP() which iterates characters and applies level-ups by subtracting charLVL*100 from charXP and incrementing charLVL when thresholds are met. Minor whitespace cleanup included.

--- frog-start/Managers/c_manager.gd
+++ frog-start/Managers/c_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 @export var party:=[1,0,0,0]
 @export var charUnlocked=[1,1,1,1,1,1,1,1,1,0]
-@export var charXP=[100,0,0,0,0,0,0,0,0,0]
-@export var charLVL=[20,20,20,20,20,20,20,20,20,0]
+@export var charXP=[100,200,300,500,1000,0,0,0,0,0]
+@export var charLVL=[1,1,1,1,1,1,1,1,1,1]
 @export var charHP=[200,175,225,275,320,360,400,600,225,999]
+
+
+func updateXP()->void:
+	for f in range(charXP.size()-1):
+		if charXP[f]>=charLVL[f]*100:
+			charXP[f]-=(charLVL[f]*100)
+			charLVL[f]+=1
