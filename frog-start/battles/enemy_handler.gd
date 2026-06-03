d9631fd460fec33ee94120bbbb9b3e942bf6f005
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-06-03 17:53:54 -0500

    Distribute enemy counts into curEnems slots
    Remove the unused global allowance and initialize a local allowance in _ready(). Allocate random enemy groups (2–3) into the first available curEnems slots while allowance/3.0 > 1, then fill remaining allowance by placing single enemies into empty slots. Add debug prints for allowance and curEnems. This ensures enemy counts are assigned across up to four slots on startup.

--- frog-start/battles/enemy_handler.gd
+++ frog-start/battles/enemy_handler.gd
@@ -1,18 +1,26 @@
 extends Node2D
 
-var allowance:=0
 var aliveEnemies:=0
 var curEnems=[0,0,0,0]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	allowance=randi_range(2,5)
-	while abs(allowance/3)>1:
+	var allowance=7
+	print(allowance)
+	while allowance/3.0>1:
 		var curEnem=randi_range(2,3)
 		allowance-=curEnem
+		for i in range(4):
+			if curEnems[i]==0:
+				curEnems[i]=curEnem
+				break
 	while allowance>0:
 		allowance-=1
-	
+		for i in range(4):
+			if curEnems[i]==0:
+				curEnems[i]=1
+				break
+	print(curEnems)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
